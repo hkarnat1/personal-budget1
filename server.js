7dce4e54f76f20e7994bc2cb571fb17e679e36d0
harshini98 <karnatiharshini@gmail.com>
2023-09-16 23:20:30 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,30 +1,52 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const budgetJSON=require('./budget.json');
 app.use('/', express.static('public'));
-const budget = {
-    my_budget: [
-    {
-        title: 'Eat out',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-    },
-]
-};
+// const budget = {
+//     my_budget: [
+//     {
+//         title: 'Eat out',
+//         budget: 500
+//     },
+//     {
+//         title: 'Rent',
+//         budget: 900
+//     },
+//     {
+//         title: 'Groceries',
+//         budget: 200
+//     },
+//     {
+//         title: 'Investment',
+//         budget: 2000
+//     },
+//     {
+//         title: 'Savings',
+//         budget: 2500
+//     },
+//     {
+//         title: 'Trips',
+//         budget: 1500
+//     },
+//     {
+//         title: 'EMI',
+//         budget: 450
+//     },
+//     {
+//         title: 'Miscellaneous',
+//         budget: 150
+//     },
+
+// ]
+// };
 
 
 app.get('/hello', (req, res) => {
     res.send("Hello WOrld!");
 });
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(budgetJSON);
 });
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:${port}')
