f30dc216e2c8e1dd3fe716384578dce1e6114de6
hkarnat1 <hkarnat1@uncc.edu>
2023-10-15 20:27:20 -0400

    read data from mongodb

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/your-database-name', { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = "mongodb://localhost:27017/budget";
+mongoose.connect(uri);
 const app = express();
-const port = 8080;
-const budgetJSON=require('./budget.json');
+const port = 3001;
+app.use(express.json())
+// const budgetJSON=require('./budget.json');
 app.use('/', express.static('public'));
 // const budget = {
 //     my_budget: [
@@ -41,13 +46,50 @@ app.use('/', express.static('public'));
 // ]
 // };
 
+const budgetSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    relatedValue: {
+      type: Number,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+      validate: /^#([0-9a-fA-F]{6})$/,
+    },
+  });
+  const budget_data = mongoose.model('budget_data', budgetSchema);
 
+app.get('/api/fetchBudget', async(req, res) => {
+    const budget = await budget_data.find();
+    res.json(budget);
+});
+
+app.post('/api/addBudget', async (req, res) => {
+    // const budget_data = new my_budget({
+    //     title: req.body.title,
+    //     budget: req.body.budget
+    // })
+    try{
+    const { title, relatedValue, color } = req.body;
+    const budget_item = new budget_data({ title, relatedValue, color });
+    const savedEntry = await budget_item.save();
+    res.json(savedEntry);
+    }
+    catch(error) {
+        res.status(500).send(error);
+    }
+    
+  });
 app.get('/hello', (req, res) => {
     res.send("Hello WOrld!");
 });
-app.get('/budget', (req, res) => {
-    res.json(budgetJSON);
-});
+// app.get('/budget', (req, res) => {
+//     res.json(budgetJSON);
+// });
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:${port}')
 })
